8cf1c1ac16ae7433cab412bb725bcddd1eaf44cf
yashwant gahlot <yashwantgahlot@yashwants-MacBook-Pro.local>
2023-05-22 18:06:06 +0530

    exceptions code rectified

--- src/main/java/com/example/Wallet/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/Wallet/exception/ResourceNotFoundException.java
@@ -3,6 +3,14 @@ package com.example.Wallet.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//when the ResourceNotFoundException is thrown, the Spring MVC framework will handle
+// the exception based on its default exception handling mechanism.
+// It will look for an appropriate exception handler method to handle the exception and
+// determine the response status code. If no custom exception handler is found,
+// the response will have an HTTP status code of 500 (Internal Server Error) by default.
+
+//the response sent back to the client will have an HTTP status code of 404 (Not Found).
+// The error message will be included in the response body.
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
