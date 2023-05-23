74e35b462abc47f06f53041270a8d14c19fd6f31
yashwant gahlot <yashwantgahlot@yashwants-MacBook-Pro.local>
2023-05-23 07:40:23 +0530

    javadoc used for the 1st time for documentation

--- src/main/java/com/example/Wallet/jwt/JwtUtil.java
+++ src/main/java/com/example/Wallet/jwt/JwtUtil.java
@@ -23,25 +23,48 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 
+/**
+ * This class handles token generation, validation, and extraction of user details from the token.
+ */
 @Service
 public class JwtUtil {
 
-	private String secret;
-	private int jwtExpirationInMs=5 * 60 * 60;
 
-	@Value("${jwt.secret}")
+	private String secret;// Represents the secret key used for signing and verifying the JWT.
+	private int jwtExpirationInMs=5 * 60 * 60;//// Represents the expiration time of the JWT in milliseconds.
+
+	//Use @Value annotation to inject the value of the jwt.secret property from external configuration into the secret variable.Two ways of using are mentioned here.
+	@Value("${jwt.secret}") //its value is stored in application.properties file
 	public void setSecret(String secret) {
 		this.secret = secret;
 	}
 	
-//	@Value("${jwt.expirationDateInMs}")
-//	public void setJwtExpirationInMs(int jwtExpirationInMs) {
-//		this.jwtExpirationInMs = jwtExpirationInMs;
-//	}
+
+	public void setJwtExpirationInMs(@Value("${jwt.expirationDateInMs}") int jwtExpirationInMs) {
+		this.jwtExpirationInMs = jwtExpirationInMs;
+	}
 
 	// generate token for user
+
+	//he UserDetails interface is part of the Spring Security framework and represents the user details
+	// required for authentication and authorization. It contains information such as the user's username, password, and authorities (roles).
+	/**
+	 * Generates a token for the given user details.
+	 * It creates a claims map to store additional information in the token, such as user roles.
+	 *
+	 * @param userDetails The user details.
+	 * @return The generated JWT token.
+	 */
 	public String generateToken(UserDetails userDetails) {
+		//It creates a claims map to store additional information in the token, such as user roles.
 		Map<String, Object> claims = new HashMap<>();
+		//The getAuthorities() method returns a collection of GrantedAuthority objects, which represent the authorities/roles assigned to the user.
+		// The returned collection typically contains instances of the SimpleGrantedAuthority class,
+		// which is a basic implementation of the GrantedAuthority interface.
+
+		//By using the wildcard ? extends GrantedAuthority, the code allows for flexibility in the type of authorities returned by the getAuthorities() method.
+		// It allows the collection to contain any class that extends the GrantedAuthority interface, providing compatibility
+		// with different implementations or custom authority classes.
 		Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
 		if (roles.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
 			claims.put("isAdmin", true);
@@ -52,12 +75,30 @@ public class JwtUtil {
 		return doGenerateToken(claims, userDetails.getUsername());
 	}
 
+	/**
+	 * Generates a JWT token using the provided claims and subject (user identifier).
+	 * It sets the issued and expiration dates for the token.
+	 * The token is signed using the HMAC-SHA512 algorithm with the secret key.
+	 * The compact token string is returned.
+	 *
+	 * @param claims  The claims to include in the token.
+	 * @param subject The subject (user identifier) for the token.
+	 * @return The generated JWT token.
+	 */
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
-
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs)).signWith(SignatureAlgorithm.HS512, secret).compact();
 	}
 
+	/**
+	 * Validates the authenticity and integrity of the given JWT token.
+	 * It attempts to parse and verify the token using the secret key.
+	 * If the token is valid, it returns true; otherwise, it throws a BadCredentialsException with an appropriate message.
+	 *
+	 * @param authToken The JWT token to validate.
+	 * @return True if the token is valid, false otherwise.
+	 * @throws BadCredentialsException If the token is invalid or expired.
+	 */
 	public boolean validateToken(String authToken) {
 		 //final String username = getUsernameFromToken(authToken);
 	      // return (username.equals(userDetails.getUsername()) && !isTokenExpired(authToken));
@@ -72,13 +113,27 @@ public class JwtUtil {
 			throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
 		}
 	}
-	
+
+	/**
+	 * Extracts the username (subject) from the given JWT token.
+	 * It parses the token, retrieves the claims, and returns the subject.
+	 *
+	 * @param token The JWT token.
+	 * @return The username extracted from the token.
+	 */
 	public String getUsernameFromToken(String token) {
 		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-
 		return claims.getSubject();
 	}
 
+	/**
+	 * Extracts the user roles from the given JWT token.
+	 * It parses the token, retrieves the claims, and checks if the "isAdmin" or "isUser" flag is present.
+	 * Based on the flags, it creates and returns a list of SimpleGrantedAuthority objects representing the user roles.
+	 *
+	 * @param authToken The JWT token.
+	 * @return The list of user roles extracted from the token.
+	 */
 	public List<SimpleGrantedAuthority> getRolesFromToken(String authToken) {
 		List<SimpleGrantedAuthority> roles = null;
 		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken).getBody();
